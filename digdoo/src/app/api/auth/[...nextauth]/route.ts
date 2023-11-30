908568e63350d063a69764a60fa241280f47efde
James Richmond <jblackenmusic@gmail.com>
2023-11-30 12:49:51 -0700

    added business area

--- digdoo/src/app/api/auth/[...nextauth]/route.ts
+++ digdoo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,30 @@
-import NextAuth from "next-auth/next";
+import { NextResponse } from "next/server"
 
-import GitHubProvider from "next-auth/providers/github";
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const { email, username, password } = body;
+        return NextResponse.json(body)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-export const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? ","
-        }),
-    ],
-};
 
-export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// import NextAuth from "next-auth/next";
+
+// import GitHubProvider from "next-auth/providers/github";
+
+// export const authOptions = {
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID ?? "",
+//             clientSecret: process.env.GITHUB_SECRET ?? ","
+//         }),
+//     ],
+// };
+
+// export const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
--- digdoo/src/app/business/page.tsx
+++ digdoo/src/app/business/page.tsx
@@ -1,11 +1,41 @@
 import BusinessNav from "./_bComponents/_bNav";
 
 export default function BusinessHome() {
+
+    const SettingsButton = ({ text }: { text:string }) => {
+        return (
+            <div className="w-[250px] h-[250px] flex items-center justify-center">
+                <div className="bg-white hover:bg-gray-100 hover:cursor-pointer h-[200px] w-[200px] hover:h-[230px] hover:w-[230px] transition-all rounded-full flex justify-center">   
+                    <p className="self-center mb-4">{text}</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <main className="flex flex-row">
-            <BusinessNav />
-            <div>
-                <p>this is the main business area</p>
+        <main className="flex flex-col bg-gray-100 h-screen">
+            <div className="h-12 bg-blue-500">
+            </div>
+            <span className="flex flex-row justify-center p-8">
+                <SettingsButton text={"View Website"} />
+                <SettingsButton text={"Update Website"} />
+                <SettingsButton text={"Website Settings"} />
+                <SettingsButton text={"Billing"} />
+            </span>
+            <div className="flex flex-col p-2">
+                <div className="flex flex-row justify-center gap-2">
+                    <div className="bg-white w-[75%]">
+                        <p>this is the table area</p>
+                    </div>
+                    <div className="flex flex-col w-[25%] gap-2">
+                        <div className="bg-white">
+                            <p>this is the pie chart</p>
+                        </div>
+                        <div className="bg-white">
+                            <p>this is the bottom right menu</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
